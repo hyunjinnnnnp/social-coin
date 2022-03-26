836d7d553cbe55fee1d4ee7427386ab89c5837a0
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-03-26 12:00:41 +0900

    VictoryChart

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { VictoryChart, VictoryLine, VictoryScatter } from "victory-native";
 import { history, info } from "../api";
 import { Icon } from "../components/Coin";
 
-const Container = styled.ScrollView``;
+const Container = styled.ScrollView`
+  background-color: black;
+`;
 
 const Detail = ({
   route: {
@@ -31,8 +34,36 @@ const Detail = ({
     ["coinHistory", id],
     history
   );
-  console.log(infoData);
-  return <Container />;
+  const [victoryData, setVictoryData] = useState(null);
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price,
+        }))
+      );
+    }
+  }, [historyData]);
+
+  return (
+    <Container>
+      {victoryData ? (
+        <VictoryChart height={360}>
+          <VictoryLine
+            animate
+            interpolation="monotoneX"
+            data={victoryData}
+            style={{ data: { stroke: "#1abc9c" } }}
+          />
+          <VictoryScatter
+            data={victoryData}
+            style={{ data: { fill: "#1abc9c" } }}
+          />
+        </VictoryChart>
+      ) : null}
+    </Container>
+  );
 };
 
 export default Detail;
